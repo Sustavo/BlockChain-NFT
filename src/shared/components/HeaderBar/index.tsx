e410e6f470389e250ee007c815ef088f12c83b78
Sustavo <gust4v0309@gmail.com>
2022-11-20 16:05:22 -0300

    feat: Create Component: HeaderBar

---
+++ src/shared/components/HeaderBar/index.tsx
@@ -0,0 +1,12 @@
+import { Container, HeaderBarText } from "./styles";
+
+export default function HeaderBar() {
+    return(
+        <Container>
+            <HeaderBarText>explore</HeaderBarText>
+            <HeaderBarText>trending nftS</HeaderBarText>
+            <HeaderBarText>auctioned nfts</HeaderBarText>
+            <HeaderBarText>influencers</HeaderBarText>
+        </Container>
+    )
+}
