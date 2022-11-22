75d193270111cdac349b0be87eb8cdc014cc1c24
Sustavo <gust4v0309@gmail.com>
2022-11-21 23:06:32 -0300

    feat: BuyNft styles

---
+++ src/shared/components/BuyNft/styles.ts
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    width: 394px;
+    height: 543px;
+    background: linear-gradient(147.75deg, rgba(255, 255, 255, 0.1) 0%, rgba(255, 255, 255, 0.05) 100%);
+    backdrop-filter: blur(10px);
+    border-radius: 15px;
+
+    margin: 10px;
+    padding: 10px 12px;
+`
+
+export const BuyNftImage = styled.img`
+    width: 370px;
+    height: 261px;
+    border-radius: 15px;
+`
