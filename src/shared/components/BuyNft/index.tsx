f2b18ff2d2b7307fd6fdbbffedbb969c1bc3711c
Sustavo <gust4v0309@gmail.com>
2022-11-22 21:43:18 -0300

    style: implement new components in BuyNft

--- src/shared/components/BuyNft/index.tsx
+++ src/shared/components/BuyNft/index.tsx
@@ -1,5 +1,6 @@
-import { BuyNftImage, Container } from "./styles";
+import { BuyNftFavorite, BuyNftFavoriteImage, BuyNftImage, Container } from "./styles";
 import MonkayApe from "../../img/BuyNft/Monkey Ape.svg"
+import Hearth from "../../img/BuyNft/Hearth.svg"
 
 export default function BuyNft() {
     return(
--- src/shared/components/BuyNft/styles.ts
+++ src/shared/components/BuyNft/styles.ts
@@ -20,3 +20,43 @@ export const BuyNftImage = styled.img`
     height: 261px;
     border-radius: 15px;
 `
+
+export const BuyNftFavorite = styled.div`
+    width: 96px;
+    height: 50px;
+    padding: 15px 20px;
+    gap: 10px;
+
+    background: linear-gradient(147.75deg, rgba(255, 255, 255, 0.1) 0%, rgba(255, 255, 255, 0.05) 100%);
+    backdrop-filter: blur(10px);
+    border-radius: 0px 15px;
+`
+
+export const BuyNftFavoriteImage = styled.img`
+    width: 17px;
+    height: 15px;
+
+`
+
+export const BuyNftFavoriteText = styled.p`
+    font-size: 16px;
+    font-weight: 400;
+    
+`
+
+export const BuyNftPerfilImage = styled.img`
+    width: 40px;
+    height: 40px;
+`
+
+export const BuyNftPerfilTitle = styled.p`
+    font-size: 25px;
+    font-weight: 400;
+    color: #FFFFFF;
+`
+
+export const BuyNftPerfilText = styled.p`
+    font-size: 14px;
+    font-weight: 400;
+    color: #FFFFFF;
+`
