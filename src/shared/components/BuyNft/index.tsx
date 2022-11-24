d23655e87ca1b11597f06bb224427b3de85e01d4
Sustavo <gust4v0309@gmail.com>
2022-11-23 21:47:57 -0300

    feat: implement Buynft "Perfil" and finalize "Img"

--- src/shared/components/BuyNft/index.tsx
+++ src/shared/components/BuyNft/index.tsx
@@ -1,11 +1,30 @@
-import { BuyNftFavorite, BuyNftFavoriteImage, BuyNftImage, Container } from "./styles";
+import { BuyNftFavorite, BuyNftFavoriteImage, BuyNftFavoriteText, BuyNftImage, BuyNftPerfilImage, BuyNftPerfilText, BuyNftPerfilTitle, Container, ContainerImg, ContainerPerfil, ContainerPerfilText } from "./styles";
 import MonkayApe from "../../img/BuyNft/Monkey Ape.svg"
 import Hearth from "../../img/BuyNft/Hearth.svg"
+import Avatar from "../../img/Avatar.svg"
+import { Flex } from "../../../globaStyle";
 
 export default function BuyNft() {
     return(
-        <Container>
-            <BuyNftImage src={MonkayApe} />
-        </Container>
+            <Container>
+                <ContainerImg>
+                    <BuyNftImage src={MonkayApe} />    
+                    <BuyNftFavorite>
+                        <BuyNftFavoriteImage src={Hearth} />
+                        <BuyNftFavoriteText>314</BuyNftFavoriteText>
+                    </BuyNftFavorite>
+                </ContainerImg>
+                <ContainerPerfil>
+                    <BuyNftPerfilImage src={Avatar} />
+                    <ContainerPerfilText>
+                        <BuyNftPerfilTitle>Monkey Ape</BuyNftPerfilTitle>
+                        <BuyNftPerfilText>By @JSmith</BuyNftPerfilText>
+                    </ContainerPerfilText>
+                </ContainerPerfil>
+
+            </Container>
+
+
+
     )
 }
