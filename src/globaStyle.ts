834a4f60b454c8c199852cdcd5ff00141518d56f
Sustavo <gust4v0309@gmail.com>
2022-11-23 21:47:22 -0300

    style: atualize styledComponent "Flex"

--- src/globaStyle.ts
+++ src/globaStyle.ts
@@ -12,10 +12,12 @@ export const globalStyle = createGlobalStyle`
   }
 `
 
-export const Flex = styled('div')<{itemAlign: string, direction: string}>`
+export const Flex = styled('div')<{alignItems?: string, flexDirection?: string, justifyContent?: string}>`
   display: flex;
-  flex-direction: ${props => props.direction ? 'row' : 'row' ? 'row-reverse' : 'row-reverse' ? 'column' : 'column' ? 'column-reverse' : 'column-reverse' ? undefined : 'normal'};
-  align-items: ${props => props.itemAlign ? 'center' : 'center' ? 'flex-start' : 'flex-start' ? 'flex-end' : 'flex-end' ? 'stretch' : 'stretch' ? 'start' : 'start' ? 'end' : 'end' ? 'baseline' : 'baseline' ? undefined : 'normal'};
+  background: transparent;
+  flex-direction: ${props => props.flexDirection ? 'row' : 'row' ? 'row-reverse' : 'row-reverse' ? 'column' : 'column' ? 'column-reverse' : 'column-reverse' ? undefined : 'normal'};
+  align-items: ${props => props.alignItems ? 'center' : 'center' ? 'flex-start' : 'flex-start' ? 'flex-end' : 'flex-end' ? 'stretch' : 'stretch' ? 'start' : 'start' ? 'end' : 'end' ? 'baseline' : 'baseline' ? undefined : 'normal'};
+  justify-content: ${props => props.justifyContent ? 'center' : 'center' ? 'start' : 'start' ? 'end' : 'end' ? 'flex-start' : 'flex-start' ? 'flex-end' : 'flex-end' ? 'left' : 'left' ? 'right' : 'right' ? 'space-between' : 'space-between' ? 'space-around' : 'space-around' ? 'space-evenly' : 'space-evenly' ? 'stretch' : 'stretch'};
 `
 
 export default globalStyle;
