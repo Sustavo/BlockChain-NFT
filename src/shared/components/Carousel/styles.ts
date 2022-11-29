a571b588670f18fe390421d4799c2f62bd550b42
Sustavo <gust4v0309@gmail.com>
2022-11-29 00:51:49 -0300

    style: Carousel Css-in-Js Components

---
+++ src/shared/components/Carousel/styles.ts
@@ -0,0 +1,39 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    display: flex;
+    align-items: center;
+
+`
+
+//Images
+export const CarouselImg = styled.img`
+    width: 410px;
+    height: 546px;
+    /* border-radius: 15px; */
+    
+    z-index: 3;
+`
+
+export const CarouselsideImg = styled.img`
+    width: 348px;
+    height: 464px;
+    /* border-radius: 15px; */
+
+    z-index: 2;
+`
+
+export const CarouselsidetwoImg = styled.img`
+    width: 285px;
+    height: 380px;
+    /* border-radius: 15px; */
+    
+    z-index: 1;
+`
+
+export const CarouselsidethreeImg = styled.img`
+    width: 225px;
+    height: 300px;
+    /* border-radius: 15px; */
+
+`
