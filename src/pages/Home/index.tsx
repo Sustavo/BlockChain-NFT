0e5bc69e39b77f5de4618c444f7661d2343fee6d
Sustavo <gust4v0309@gmail.com>
2022-11-29 23:50:40 -0300

    att home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,20 +1,22 @@
 //Components
-import WalletsSupportFull from "../../shared/components/WalletsSupportFull"
-import NftHeaderFull from "../../shared/components/NftHeaderFull"
-import HowWork from "../../shared/components/HowWork"
-import ConnectWallet from "../../shared/components/ConnectWallet"
-import HeaderBar from "../../shared/components/HeaderBar"
-import ConnectWalletButton from "../../shared/components/ConnectWalletButton"
 import BuyNft from "../../shared/components/BuyNft"
 import Carousel from "../../shared/components/Carousel"
+import ConnectWallet from "../../shared/components/ConnectWallet"
+import ConnectWalletButton from "../../shared/components/ConnectWalletButton"
+import CreateButton from "../../shared/components/CreateButton"
+import ExploreMoreButton from "../../shared/components/ExploreMoreButton"
+import HeaderBar from "../../shared/components/HeaderBar"
+import HowWork from "../../shared/components/HowWork"
 import JoinedInfluencers from "../../shared/components/JoinedInfluencers"
+import NftHeaderFull from "../../shared/components/NftHeaderFull"
+import WalletsSupportFull from "../../shared/components/WalletsSupportFull"
 //CSS
 import './home.component.css'
 
 export default function Home(){
     return (
         <>
-            <JoinedInfluencers />
+            <CreateButton />
         </>
     )
 }
