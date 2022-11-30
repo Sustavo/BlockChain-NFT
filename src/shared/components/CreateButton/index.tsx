463e1ac77e961c72736fdcbec7ebdeb5d8174944
Sustavo <gust4v0309@gmail.com>
2022-11-29 23:50:00 -0300

    feat: create and finalize "CreateButton" Component

---
+++ src/shared/components/CreateButton/index.tsx
@@ -0,0 +1,12 @@
+import { Bar, Button, Container } from "./styles";
+
+export default function CreateButton() {
+    return(
+        <Container>
+            <Bar />
+            <Button>Create</Button>
+            <Bar />
+        </Container>
+    )
+
+}
