23561a818ffc824df6e5a893b3044bee2dc0ae6f
Sustavo <gust4v0309@gmail.com>
2022-11-29 23:50:21 -0300

    style: CreateButton css-in-Js component

---
+++ src/shared/components/CreateButton/styles.ts
@@ -0,0 +1,30 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+    display: flex;
+    align-items: center;
+
+`
+
+export const Button = styled.button`
+    width: 215px;
+    height: 70px;
+
+    background: rgba(173, 26, 175, 0.05);
+    border: 1px solid #AD1AAF;
+
+    color: #FFFFFF;
+    font-weight: 500;
+    font-size: 22px;
+    cursor: pointer;
+`
+
+export const Bar = styled.div`
+    width: 4.6px;
+    height: 42px;
+
+    background: #AD1AAF;
+
+    cursor: pointer;
+
+`
